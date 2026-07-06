fa69453d2aefda2534de2260a51578111dbc4f62
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-07-06 19:48:49 +0900

    feat: 푸시 알람 Entity 모듈 등록

---
+++ src/alarms/alarms.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserAlarmEntity } from "./entities/user-alarm.entity";
+import { PushTokenEntity } from "./entities/push-token.entity";
+import { ReminderEntity } from "./entities/reminder.entity";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([UserAlarmEntity, PushTokenEntity, ReminderEntity])],
+    controllers: [],
+    providers: [],
+})
+export class AlarmModule {}
