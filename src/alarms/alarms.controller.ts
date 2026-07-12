0a99d7ec8b0befa8df195c465a19952419918f5f
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-07-13 00:06:14 +0900

    Feat: 알람 컨트롤러 초기 구현

---
+++ src/alarms/alarms.controller.ts
@@ -0,0 +1,70 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { AlarmsService } from "./alarms.service";
+import { PushTokenRequestDto, UpdateAlarmDto, UpdateReminderDto } from "./alarms.dto";
+import { ApiTags } from "@nestjs/swagger";
+import { DeletePushTokenSwagger, FindOneAlarmSwagger, FindOneReminderSwagger, RegisterPushTokenSwagger, UpdateAlarmSwagger, UpdateReminderSwagger } from "./alarms.swagger";
+
+@ApiTags('alarms')
+@Controller('alarms')
+export class AlarmsController {
+
+    constructor(
+        private readonly alarmsService: AlarmsService
+    ) {}
+
+    // 알람 설정 조회
+    @FindOneAlarmSwagger()
+    @Get()
+    async alarmAll() {
+        const userId = 1; //나중에 사용자 인증 데코레이터로 교체
+        return this.alarmsService.findOneAlarmEntity(userId);
+    }
+
+    // 알람 설정 수정
+    @UpdateAlarmSwagger()
+    @Patch()
+    async alarmUpdate(
+        @Body() updateAlarmDto: UpdateAlarmDto,
+    ) {
+        const userId = 1; //나중에 사용자 인증 데코레이터로 교체
+        return this.alarmsService.updateAlarmEntity(userId, updateAlarmDto);
+    }
+
+    // 리마인드 알람 설정 조회
+    @FindOneReminderSwagger()
+    @Get('/reminder')
+    async reminderAll() {
+        const userId = 1; //나중에 사용자 인증 데코레이터로 교체
+        return this.alarmsService.findOneReminderEntity(userId);
+    }
+
+    // 리마인드 알람 설정 수정
+    @UpdateReminderSwagger()
+    @Patch('/reminder')
+    async reminderUpdate(
+        @Body() updateReminderDto: UpdateReminderDto,
+    ) {
+        const userId = 1; //나중에 사용자 인증 데코레이터로 교체
+        return this.alarmsService.updateReminderEntity(userId, updateReminderDto);
+    }
+
+    // FCM 토큰 등록 (앱 로그인/실행 시)
+    @RegisterPushTokenSwagger()
+    @Post('/push-token')
+    async registerPushToken(
+        @Body() pushTokenRequestDto: PushTokenRequestDto,
+    ) {
+        const userId = 1; //나중에 사용자 인증 데코레이터로 교체
+        return this.alarmsService.registerPushToken(userId, pushTokenRequestDto);
+    }
+
+    // FCM 토큰 삭제 (앱 로그아웃 시)
+    @DeletePushTokenSwagger()
+    @Delete('/push-token/:tokenId')
+    async deletePushToken(
+        @Param('tokenId') tokenId: number,
+    ) {
+        const userId = 1 //나중에 사용자 인증 데코레이터로 교체
+        return this.alarmsService.deletePushToken(userId, tokenId);
+    }
+}
