d68c639d09cd6ebd9587a11b7478f4c0eb6dec4f
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-07-13 00:06:36 +0900

    Feat: 알람 관련 DTO 초기 구현

---
+++ src/alarms/alarms.dto.ts
@@ -0,0 +1,77 @@
+import { Transform } from "class-transformer";
+import { IsArray, IsBoolean, IsEnum, IsNumber, IsOptional, IsString } from "class-validator";
+import { DeviceType } from "./entities/push-token.entity";
+
+
+// 알람 설정 조회 응답 DTO
+export class AlarmResponseDto {
+    // PUSH 알람 여부 
+    isPush: boolean;  
+    // 일기 작성 알람 여부
+    isDiary: boolean;
+    // 일기 답장 알람 여부
+    isDiaryReply: boolean;
+}
+
+// 리마인드 조회 응답 DTO
+export class ReminderResponseDto {
+    // 알람 요일
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    days: string[] | null;
+
+    // 알람 시간
+    @IsOptional()
+    time: string | null;
+}
+
+// 알람 설정 수정 DTO
+export class UpdateAlarmDto {
+    //PUSH 알람 여부 
+    @IsOptional()
+    @Transform(({ value }) => value === true ||value === 'true')
+    @IsBoolean()
+    isPush?: boolean;
+
+    // 일기 작성 알람 여부
+    @IsOptional()
+    @Transform(({ value }) => value === true ||value === 'true')
+    @IsBoolean()
+    isDiary?: boolean;
+
+    // 일기 답장 알람 여부
+    @IsOptional()
+    @Transform(({ value }) => value === true ||value === 'true')
+    @IsBoolean()
+    isDiaryReply?: boolean;
+}
+
+// 리마인드 설정 수정 DTO
+export class UpdateReminderDto {
+    // 알람 요일
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    days?: string[] | null;
+
+    // 알람 시간
+    @IsOptional()
+    @IsString()
+    time?: string | null;
+}
+
+// FCM 토큰 요청 
+export class PushTokenRequestDto {
+    @IsString()
+    fcmToken: string;
+
+    @IsEnum(DeviceType)
+    deviceType: DeviceType;
+}
+// FCM 토큰 응답
+export class PushTokenResponseDto {
+    tokenId: number;
+    fcmToken: string;
+    deviceType: DeviceType;
+}
