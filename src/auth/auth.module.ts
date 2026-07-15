9c235741c4513033747a18c38f03b1ed5f01858a
성태경 <taegyeong0320@naver.com>
2026-07-16 03:24:33 +0900

    Feat: 이메일 인증 엔티티 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,21 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../users/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { EmailVerificationEntity } from './entities/email-verification.entity';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule, JwtModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule,
+    TypeOrmModule.forFeature([EmailVerificationEntity]),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy, JwtAuthGuard],
   exports: [JwtAuthGuard],
