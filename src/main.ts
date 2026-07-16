888685425bdd49919eea177df6ee18c6ec9743f8
성태경 <taegyeong0320@naver.com>
2026-07-17 03:45:28 +0900

    Refactor: 인증 설정 및 헬스체크 엔드포인트 정리

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,6 @@ import { ResponseInterceptor } from './global/common/response.interceptor';
 async function bootstrap() {
   // const isProd = process.env.NODE_ENV === 'production';
 
-  if (!process.env.SESSION_SECRET) {
-    throw new Error('SESSION_SECRET 값이 없습니다.');
-  }
-
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useGlobalFilters(new GlobalExceptionFilter());
@@ -27,7 +23,6 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['http://localhost:8000', 'http://localhost:3000'], //배포 URL 추후 설정
-    credentials: true,
   });
 
   app.setGlobalPrefix('api/v1');
@@ -39,7 +34,6 @@ async function bootstrap() {
     .setTitle('API')
     .setDescription('API Documentation')
     .setVersion('1.0')
-    .addCookieAuth('connect.sid')
     .addBearerAuth(
       {
         type: 'http',
@@ -55,11 +49,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document, {
-    swaggerOptions: {
-      withCredentials: true,
-    },
-  });
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(Number(process.env.BACKEND_PORT) || 3000);
 }
