62e43dd65516c410528c4114cfe06373ea579862
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-07-20 22:52:13 +0900

    Chore: 서버 루트 경로 헬스체크 응답 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,14 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('health')
-@Controller('health')
+@Controller()
 export class AppController {
   @Get()
   getHealth() {
     return {
-      message: '서버 실행 확인 성공',
+      resultType: 'SUCCESS',
+      message: '성공',
+      data: '백엔드 화이팅!!!!!!!!!',
     };
   }
 }
