0b56641f00b1ff51218cb9ff6fb1bfa6cc9caf17
soyerin0407 <hstepk472@naver.com>
2026-07-27 16:42:36 +0900

    [fix/#174] utc for diary date check

--- src/diaries/diary.service.ts
+++ src/diaries/diary.service.ts
@@ -73,13 +73,13 @@ export class DiaryService {
   console.log('questionId == null:', dto.questionId == null);
   console.log('==================================');
 
-  const now = new Date();
+ const now = new Date();
 
 const startOfDay = new Date(now);
-startOfDay.setHours(0, 0, 0, 0);
+startOfDay.setUTCHours(0, 0, 0, 0);
 
 const endOfDay = new Date(now);
-endOfDay.setHours(23, 59, 59, 999);
+endOfDay.setUTCHours(23, 59, 59, 999);
 
 const todayDiary = await this.diaryRepository.findOne({
   where: {
