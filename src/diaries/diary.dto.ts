acf8e8e56ad2231f495e4c0dbbb94e6cf2467f94
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-07-29 04:27:27 +0900

    Feat: 일기 시간 관련 로직 적용

--- src/diaries/diary.dto.ts
+++ src/diaries/diary.dto.ts
@@ -5,12 +5,13 @@ import {
   IsNumber,
   IsOptional,
   IsString,
-  IsDateString,
   MaxLength,
 } from 'class-validator';
 
-import {Transform} from 'class-transformer';
+import { Transform } from 'class-transformer';
+import { ToLocalDate } from '../global/decorators/date.decorator';
 
+// 일기 작성 요청 DTO
 export class CreateDiaryDto {
   @ApiProperty({
     description: '일기 제목',
@@ -36,30 +37,42 @@ export class CreateDiaryDto {
   })
   @IsOptional()
   @Transform(({ value }) =>
-  value === '' || value == null ? undefined : Number(value),
-)
+    value === '' || value == null ? undefined : Number(value),
+  )
   @IsNumber()
   questionId?: number;
 
-@ApiPropertyOptional({
-  description: '업로드 이미지',
-  type: 'string',
-  format: 'binary',
-  isArray: true,
-})
-@IsOptional()
-images?: any;
+  @ApiPropertyOptional({
+    description: '업로드 이미지',
+    type: 'string',
+    format: 'binary',
+    isArray: true,
+  })
+  @IsOptional()
+  images?: any;
 
-@ApiProperty({
-  example: '2026-07-28T00:30:00+09:00',
-  description: '사용자 로컬 기준 작성 시각',
-})
-@IsDateString()
-date: string;
-
-
+  @ApiProperty({
+    example: '2026-07-28T00:30:00+09:00',
+    description: '사용자 로컬 기준 작성 시각',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @ToLocalDate()
+  date: string;
 }
 
+// 일기 작성 응답 DTO
+export class CreateDiaryResponseDto {
+  diaryId: number;
+  userId: number;
+  date: string | null;
+  diaryType: string;
+  diaryTitle: string;
+  content: string;
+  aiSummary: string | null;
+}
+
+// 일기 수정 요청 DTO
 export class UpdateDiaryDto {
   @ApiPropertyOptional({
     description: '일기 제목',
@@ -94,34 +107,27 @@ export class UpdateDiaryDto {
   images?: string[];
 }
 
-/**
- * 목록 조회 응답 DTO
- */
+// 목록 조회 응답 DTO
 export class DiaryListResponseDto {
   diaryId: number;
-
-  title: string;
-
-  createdAt: Date;
-
-  @ApiProperty({
-  description: '이미지 URL 목록',
-  type: [String],
-})
-images: string[];
+  userId: number;
+  date: string | null;
+  diaryType: string;
+  diaryTitle: string;
+  content: string;
+  aiSummary: string | null;
+  images: string[];
 }
 
-/**
- * 상세 조회 응답 DTO
- */
+// 상세 조회 응답 DTO
 export class DiaryDetailResponseDto {
   diaryId: number;
-
-  title: string;
-
+  userId: number;
+  date: string | null;
+  diaryType: string;
+  diaryTitle: string;
   content: string;
-
   aiSummary: string | null;
-
+  questionContent: string | null;
   images: string[];
 }
